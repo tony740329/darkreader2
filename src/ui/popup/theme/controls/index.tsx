331db4d8a9b513540391e08117b9355763f32653
博軒 <49735850+tony740329@users.noreply.github.com>
2022-05-23 19:20:06 +0800

    Initialize link-Patch

---
+++ src/ui/popup/theme/controls/index.tsx
@@ -0,0 +1,37 @@
+import BackgroundColor from './background-color';
+import Brightness from './brightness';
+import Contrast from './contrast';
+import FontPicker from './font-picker';
+import Grayscale from './grayscale';
+import LinkColorEditor from './link';
+import Mode from './mode';
+import ResetButton from './reset-button';
+import Scheme from './scheme';
+import Scrollbar from './scrollbar';
+import SelectionColorEditor from './selection';
+import Sepia from './sepia';
+import StyleSystemControls from './style-system-controls';
+import TextColor from './text-color';
+import TextStroke from './text-stroke';
+import UseFont from './use-font';
+import VisitedLinkColorEditor from './visited-link';
+
+export {
+    BackgroundColor,
+    Brightness,
+    Contrast,
+    FontPicker,
+    Grayscale,
+    LinkColorEditor,
+    Mode,
+    ResetButton,
+    Scheme,
+    Scrollbar,
+    SelectionColorEditor,
+    Sepia,
+    StyleSystemControls,
+    TextColor,
+    TextStroke,
+    UseFont,
+    VisitedLinkColorEditor,
+};
